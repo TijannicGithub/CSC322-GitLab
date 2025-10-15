96797605fbcf1162605f16ca8da4d82d26daee34
TijannicGithub <tsc160@miami.edu>
2025-10-15 14:28:21 -0400

    Update message to 'Hello Crazy World!

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#include <stdio.h>\nint main(){ printf("Hello Very Crool World!\\n"); return 0; }
-=======
-#include <stdio.h>\nint main(){ printf("Hello Happy World!\\n"); return 0; }
->>>>>>> Happy
+#include <stdio.h>\nint main(){ printf("Hello Crazy World!\\n"); return 0; }
